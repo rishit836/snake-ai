5bf336e16fec491d8a3b406cf02bf23927330d67
Rishit Sharma <rishitshar36@gmail.com>
2026-03-06 23:51:44 +0530

    snake game done

--- game.py
+++ game.py
@@ -11,16 +11,30 @@ WIDTH,HEIGHT = 500,500
 screen = pygame.display.set_mode((WIDTH,HEIGHT))
 pygame.display.set_caption("snake ai goess brr")
 clock = pygame.time.Clock()
-head_x,head_y = (WIDTH-head_size)/2,(HEIGHT-head_size)/2
-fruit_x,fruit_y = random.randint(0,WIDTH-head_size),random.randint(0,HEIGHT-head_size)
+
+fruit_x,fruit_y = random.randrange(0, WIDTH, head_size),random.randrange(0, HEIGHT, head_size)
 velx,vely=0,0
+score=0
+snake = [((WIDTH-head_size)//2,(HEIGHT-head_size)//2)]
+move_delay = 6
+frame_count = 0
 
+def draw_snake(screen,snake):
+    for idx,body in enumerate(snake):
+        rect_obj = pygame.Rect((body[0],body[1]),(head_size,head_size))
+        pygame.draw.rect(screen,"green",rect_obj)
+        if idx == 0:
+            head = rect_obj
 
-def draw_snake(screen,head_x,head_y):
-    rect_obj = pygame.Rect((head_x,head_y),(head_size,head_size))
-    pygame.draw.rect(screen,"green",rect_obj)
-    pygame.display.flip()
-
+    return head
+def move_snake(snake,velx,vely,grow=False):
+    head_x,head_y=snake[0]
+    head_x+=velx
+    head_y+=vely
+    snake.insert(0,(head_x,head_y))
+    if not grow:
+        snake.pop()
+    return snake
 while True:
     for event in pygame.event.get():
         if event.type== pygame.QUIT:
@@ -28,30 +42,53 @@ while True:
             sys.exit()
             break
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_RIGHT:
-                velx=5
-                vely=0
-            if event.key == pygame.K_LEFT:
-                velx=-5
-                vely=0
-            if event.key == pygame.K_UP:
-                velx=0
-                vely=-5
-            if event.key == pygame.K_DOWN:
-                velx=0
-                vely=5
+            if event.key == pygame.K_RIGHT and velx == 0:
+                velx = head_size
+                vely = 0
+
+            elif event.key == pygame.K_LEFT and velx == 0:
+                velx = -head_size
+                vely = 0
+
+            elif event.key == pygame.K_UP and vely == 0:
+                velx = 0
+                vely = -head_size
+
+            elif event.key == pygame.K_DOWN and vely == 0:
+                velx = 0
+                vely = head_size
             
 
     screen.fill("black")
-    draw_snake(screen,head_x,head_y)
-    head_x+=velx
-    head_y+=vely
+    head = draw_snake(screen,snake)
+    frame_count += 1
+
+    # draw fruit
+    fruit = pygame.Rect((fruit_x,fruit_y),(head_size,head_size))
+    pygame.draw.rect(screen,"red",fruit)
+
+    if frame_count >= move_delay:
+
+        snake = move_snake(snake, velx, vely, grow=False)
+
+        head_x, head_y = snake[0]
+
+        if fruit.colliderect(head):
+            snake.append(snake[-1])   # grow
+            fruit_x = random.randrange(0, WIDTH, head_size)
+            fruit_y = random.randrange(0, HEIGHT, head_size)
+            score += 1
+
+        frame_count = 0
+    
+
+
     pygame.display.flip()
     clock.tick(60)
     # game over
-    if not 0<=head_x<=WIDTH:
+    if not 0<=snake[0][0]<WIDTH:
         print("game over bitch!")
         break
-    if not 0<=head_y<=HEIGHT:
+    if not 0<=snake[0][1]<=HEIGHT:
         print("game over bitch!")
         break
